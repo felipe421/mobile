5605c2acecc0c5656f67023bc0538660eaa95236
Felipe Arcanjo <felipe.arcanjo@iesb.edu.br>
2023-10-31 21:00:07 -0300

    Lista de Tarefas AsyncStorage

--- formulario/src/routes/DrawerRoutes.js
+++ formulario/src/routes/DrawerRoutes.js
@@ -3,12 +3,14 @@ import React from 'react'
 import ListaCarros from '../screens/listaCarro/ListaCarros'
 import StackPessoas from '../screens/Pessoas/StackPessoas'
 import StackAlunos from '../screens/alunos/StackAlunos'
+import ListaCarrosAsyncStorage from '../screens/listaCarrosAsyncStorage/ListaCarrosAsyncStorage'
 
 const Drawer = createDrawerNavigator()
 
 export default function DrawerRoutes() {
     return (
-        <Drawer.Navigator initialRouteName='alunos'>
+        <Drawer.Navigator initialRouteName='CarrosA'>
+            <Drawer.Screen name='CarrosA' component={ListaCarrosAsyncStorage}/>
             <Drawer.Screen name='Carros' component={ListaCarros} />
             <Drawer.Screen name='Pessoas' component={StackPessoas}/>
             <Drawer.Screen name='alunos' component={StackAlunos}/>
